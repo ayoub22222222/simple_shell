133be485c15d6d1f710a8c828b9cee7cea1c8af5
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2023-11-15 15:32:47 -0800

    p

--- error_handling.c
+++ error_handling.c
@@ -38,3 +38,37 @@ int _stat(char **cmd, char **path)
 	free(path);
 	return (0);
 }
+/**
+ * handle_signal - prints new line and prompt when CTRL + C is passed as input
+ * @signal: name of sig
+ */
+void handle_signal(int signal)
+{
+	char *prompt = {"\n{^_^} "};
+	(void) signal;
+
+	write(STDOUT_FILENO, prompt, _strlen(prompt));
+	fflush(stdout);
+}
+/**
+ * _error - prints the error output of a file
+ * @argv: name of program
+ * @count: number of prompt
+ * @args: command to be put in
+ * Return: 0 on success
+ */
+int *_error(char *argv, int count, char *args)
+{
+	char *number;
+
+	number = _itoa(count, 10);
+
+	write(2, argv, _strlen(argv));
+	write(2, ": ", 2);
+	write(2, number, _strlen(number));
+	write(2, ": ", 2);
+	write(2, args, _strlen(args));
+	perror(" ");
+
+	return (0);
+}
