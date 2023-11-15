cf9893047d269346d2ec91a1ba79d297a29e8746
Zinebb12 <zinebzaid@gmail.com>
2023-11-15 15:55:35 -0800

    edits

--- helper_function.c
+++ helper_function.c
@@ -42,3 +42,59 @@ int child_process(char **av, char **args, char **env, int status_main, int cnt)
 	}
 	return (1);
 }
+
+/**
+ * search_path - gets the path to execute command
+ * @environ: Environment variable
+ * Return: token_path
+ **/
+char **search_path(char **environ)
+{
+	int position = 0;
+	char **token_path;
+
+	for (; environ[position] != NULL ; position++)
+	{
+		if (environ[position][0] == 'P' && environ[position][2] == 'T')
+		{
+			token_path = _which(environ[position]);
+		}
+	}
+	return (token_path);
+}
+
+/**
+ * _which - identifies the path of the command(*args)
+ * @foundpath: the command that is being passed to it
+ * Return: the complete path of the command or 0 on failure
+ */
+char **_which(char *foundpath)
+{
+	int size = TOK_BUFSIZE, i = 0;
+	char *copy_path = NULL, *tokens = NULL, *sparse = ":=";
+	char **dir = _calloc(sizeof(char *), size);
+
+	if (foundpath == NULL)
+	{
+		free(foundpath);
+		return (0);
+	}
+	if (dir == NULL)
+	{
+		free(foundpath);
+		perror("Error allocated memory");
+		return (NULL);
+	}
+
+	copy_path = _strdup(foundpath); /*copiar el string --> palabra PATH*/
+	tokens = strtok(copy_path, sparse); /*separar el string por un separador*/
+	while (tokens != NULL)
+	{
+		dir[i] = tokens;
+		i++;
+		tokens = strtok(NULL, sparse);
+	}
+
+	return (dir);
+}
+
